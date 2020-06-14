d9b48e8022973a0c1fefa144fa686137ae9cfe4a
apurva-deshmukh <aadeshmukh13@gmail.com>
2020-06-14 15:14:11 -0400

    Set up new conversations view controller

--- Messenger/Controllers/ConversationsViewController.swift
+++ Messenger/Controllers/ConversationsViewController.swift
@@ -34,9 +34,19 @@ class ConversationsViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .compose,
+                                                            target: self,
+                                                            action: #selector(didTapComposeButton))
         view.addSubview(tableView)
+        view.addSubview(noConversationsLabel)
         setupTableView()
         fetchConversations()
+    }
+    
+    @objc private func didTapComposeButton() {
+        let vc = NewConversationViewController()
+        let navVC = UINavigationController(rootViewController: vc)
+        present(navVC, animated: true)
     }
     
     override func viewDidLayoutSubviews() {
